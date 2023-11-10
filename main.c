6fe05a82504784ec2b1401bdeae4d92decb29289
Jean Vencigueri <107017702+VenciguerJ@users.noreply.github.com>
2023-11-09 22:41:32 -0300

    Versão merge com a main
    versão com a sua parte do código junto da main

--- main.c
+++ main.c
@@ -15,14 +15,167 @@ typedef struct{
 } user;
 
 typedef struct{
-	char endereco[999];
-	char bairro[90];	
-	char donoDoImovel[101];
-	int tamanho; //em metros quadrados
-	int valorDeVenda;
-	int ValorDeAluguel;
-	int qntdComodos;
-}imovel;
+    int nroimovel;
+	char endereco[101];
+    char bairro[101];
+    char cidade[101];
+    char estado[2];
+    char cep[8];
+	int metragemtotal;
+	int metragemconstruido;
+	int numerodequartos;
+    int numerodesiutes;
+    int numerodesalas;
+    int numerodebanheiros;
+    char tempiscina;
+    char temchurrasqueia;
+    float valor; 
+} imovel;
+
+void imprimir_dados(imovel a){
+    printf("Identificacao Imovel: %i\n", a.nroimovel);
+    printf("Endereco: %s\n", a.endereco);
+    printf("Bairro: %s\n", a.bairro);
+    printf("Cidade: %s\n", a.cidade);
+    printf("Estado: %s\n", a.estado);
+    printf("CEP: %s\n", a.cep);
+    printf("Metragem do terreno: %i\n", a.metragemtotal);
+    printf("Metragem area construida: %i\n", a.metragemconstruido);
+    printf("Quantidade de quartos: %i\n", a.numerodequartos);
+    printf("Quantidade de suites: %i\n", a.numerodesiutes);
+    printf("Quantidade de salas: %i\n", a.numerodesalas);
+    printf("Quantidade de banheiros: %i\n", a.numerodebanheiros);
+    printf("Possui piscina: %c\n", a.tempiscina);
+    printf("Possui churrasqueira: %c\n", a.temchurrasqueia);
+    printf("Valor do imovel: %f\n", a.valor);
+}
+
+void removerIMOVEL(char *arquivo, int numimovel, int cep2){
+    imovel tmp;
+    FILE* arq = fopen(arquivo,"r");
+    FILE* arqNovo = fopen("novo.txt","w"); 
+    while(!feof(arq)){
+	    fscanf(arq,"%i %s %s %s %s %s %i %i %i %i %i %i %c %c %f \n", &tmp.nroimovel, tmp.endereco, tmp.bairro, tmp.cidade, tmp.estado, tmp.cep, &tmp.metragemtotal, &tmp.metragemconstruido, &tmp.numerodequartos, &tmp.numerodesiutes, &tmp.numerodesalas, &tmp.numerodebanheiros, &tmp.tempiscina, &tmp.temchurrasqueia, &tmp.valor);
+        if (tmp.nroimovel!=numimovel && tmp.cep != cep2){
+            fprintf(arq,"%i %s %s %s %s %s %i %i %i %i %i %i %c %c %f \n", tmp.nroimovel, tmp.endereco, tmp.bairro, tmp.cidade, tmp.estado, tmp.cep, tmp.metragemtotal, tmp.metragemconstruido, tmp.numerodequartos, tmp.numerodesiutes, tmp.numerodesalas, tmp.numerodebanheiros, tmp.tempiscina, tmp.temchurrasqueia, tmp.valor);
+        }
+    }
+    fclose(arq); 
+    fclose(arqNovo); 
+    system("del imoveis.txt");
+    system("rename novoemp.txt imoveis.txt");
+}
+
+
+imovel pesquisaIMOVEL(char *arquivo, int nroimovel, int cep){
+
+	FILE *arq = fopen(arquivo, "r");
+	imovel tmp;
+	
+	while(!feof(arq)){
+		fscanf(arq,"%i %s %s %s %s %s %i %i %i %i %i %i %c %c %f \n", &tmp.nroimovel, tmp.endereco, tmp.bairro, tmp.cidade, tmp.estado, tmp.cep, &tmp.metragemtotal, &tmp.metragemconstruido, &tmp.numerodequartos, &tmp.numerodesiutes, &tmp.numerodesalas, &tmp.numerodebanheiros, &tmp.tempiscina, &tmp.temchurrasqueia, &tmp.valor);
+		if(cep2 == tmp.cep && numimovel == tmp.nroimovel){
+			break;
+		}
+		else{
+			tmp.nroimovel = -1;
+		}
+	}
+	fclose(arq);
+	return tmp;
+}
+
+int qtd_imoveis(char *arquivo){
+
+    FILE *arq = fopen(arquivo, "r");
+    imovel tmp;
+    int c = 0;
+    while (!feof(arq)){
+        fscanf(arq,"%i %s %s %s %s %s %i %i %i %i %i %i %c %c %f \n", &tmp.nroimovel, tmp.endereco, tmp.bairro, tmp.cidade, tmp.estado, tmp.cep, &tmp.metragemtotal, &tmp.metragemconstruido, &tmp.numerodequartos, &tmp.numerodesiutes, &tmp.numerodesalas, &tmp.numerodebanheiros, tmp.tempiscina, tmp.temchurrasqueia, &tmp.valor);
+        c++;
+    }
+    fclose(arq);
+    return c;
+}
+
+void imovel_cadastro_criaInfos(char *arquivo){
+    int i=0;
+    imovel tmp;
+    FILE *arq = fopen(arquivo, "a+");
+    tmp.nroimovel = qtd_imoveis(arquivo);
+	tmp.nroimovel += 1;
+
+
+    printf("Vamos iniciar o cadastro de um novo imovel, coloque as inforacoes a seguir\n\n");
+    printf("Endereco: ");
+    scanf("%s", tmp.endereco);
+    getchar(); 
+    printf("Bairro: ");
+    scanf("%s", tmp.bairro);
+    getchar(); 
+    printf("Cidade: ");
+    scanf("%s", tmp.cidade);
+    getchar(); 
+    printf("Estado: ");
+    scanf("%s", tmp.estado);
+    getchar(); 
+    do{    
+        printf("Informe o CEP com 8 digitos no seguinte formar 13214774 (sem o traco): ");
+        scanf("%s", tmp.cep);
+        if(strlen(tmp.cep) != 8){
+            printf("Ops... acho que deu algo errado, vamos tentar novamente.\n\n");
+            i = 1;
+        }
+    }while(i>0);
+
+	getchar(); 
+	printf("Metragem do terreno total: ");
+	scanf("%i", &tmp.metragemtotal);    
+	printf("Metragem da area contruida total: ");
+	scanf("%i", &tmp.metragemconstruido); 
+	printf("Numeros de quartos na casa: ");
+	scanf("%i", &tmp.numerodequartos); 
+	printf("Numeros de suites na casa: ");
+	scanf("%i", &tmp.numerodesiutes); 
+	printf("Numeros de salas na casa: ");
+	scanf("%i", &tmp.numerodesalas); 
+	printf("Numeros de banheiros na casa: ");
+	scanf("%i", &tmp.numerodebanheiros); 
+	printf("Tem piscina na casa, informa S para SIM e N para NAO [S/N]: ");
+	scanf("%c", &tmp.tempiscina); 
+	printf("Tem churrasqueira na casa, informa S para SIM e N para NAO [S/N]: ");
+	scanf("%c", &tmp.temchurrasqueia); 
+	printf("Informe o valor do preco do imovel para venda, casas decimais devem ser informas com ponto da seguinte forma R$ 200300.33: ");
+	scanf("%f", &tmp.valor);
+	
+	while (!feof(arq)){
+	fprintf(arq,"%i %s %s %s %s %s %i %i %i %i %i %i %c %c %f \n", tmp.nroimovel, tmp.endereco, tmp.bairro, tmp.cidade, tmp.estado, tmp.cep, tmp.metragemtotal, tmp.metragemconstruido, tmp.numerodequartos, tmp.numerodesiutes, tmp.numerodesalas, tmp.numerodebanheiros, tmp.tempiscina, tmp.temchurrasqueia, tmp.valor);
+	}
+
+	printf("O cadastro do imovel foi feito com sucesso, a identificacao dele e '%i' em nosso sistema, guarde a informacao", tmp.nroimovel);
+	
+}
+
+void consultaIMOVEL(char *arquivo){
+    imovel consulta;
+    int nroimovel, cep;
+    printf("Consulta de imoveis \n\n");
+    printf("Informe o codigo de identificacao do imovel: ");
+    scanf("%i", &nroimovel);
+    getchar();
+    printf("Informe o CEP do imovel: ");
+    scanf("%i", &cep);
+    consulta = pesquisar(arquivo, nroimovel, cep);
+    if(pesq.matricula!=-1)
+    {
+    printf("\n\n");
+    imprimir_dados(consulta);
+    }
+    else
+    {
+    printf("Imovel nao encontrado!\n\n");
+    }
+}
 
 
 char cpf_user[12];
